bf51a8536e0c867a2b2ec31505d40444b113fc58
Vasianandcarps <90087480+Vasianandcarps@users.noreply.github.com>
2021-09-18 10:53:52 +0300

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,6 @@ let n = prompt("Введите число");
 if (!isNaN(n)){
 let factorial = 1;
 for (let i = 1 ; i<= n ;i++){
-factorial =  i * factorial 
+factorial *= factorial 
 }
 alert(factorial);}else {alert("Укажите число")}
